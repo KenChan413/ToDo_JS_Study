ce18dd5ab3f76f26077b00ae5886622c883585b4
KenChan413 <85539296+KenChan413@users.noreply.github.com>
2021-06-11 17:36:28 +0900

    削除機能の実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(div)の親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode.parentNode);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)の親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode.parentNode);
+  });
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
